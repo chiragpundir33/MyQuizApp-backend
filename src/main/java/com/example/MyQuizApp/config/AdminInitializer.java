31677f5c51b7c1d999f4eaed904ed0cc35265426
chiragpundir33 <chiragpundir33@gmail.com>
2026-07-16 00:05:25 +0530

    Add PostgreSQL driver dependency

--- src/main/java/com/example/MyQuizApp/config/AdminInitializer.java
+++ src/main/java/com/example/MyQuizApp/config/AdminInitializer.java
@@ -4,6 +4,7 @@ import com.example.MyQuizApp.entity.User;
 import com.example.MyQuizApp.enums.Role;
 import com.example.MyQuizApp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,27 @@ public class AdminInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${ADMIN_NAME}")
+    private String adminName;
+
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
+
     @Override
     public void run(String... args) {
 
-        User admin = userRepository.findByEmail("admin@myquizapp.com");
+        User admin = userRepository.findByEmail(adminEmail);
 
         if (admin == null) {
 
             User defaultAdmin = User.builder()
-                    .name("Admin")
-                    .email("admin@myquizapp.com")
-                    .password(passwordEncoder.encode("Admin@123"))
+                    .name(adminName)
+                    .email(adminEmail)
+                    .password(passwordEncoder.encode(adminPassword))
                     .role(Role.ADMIN)
                     .isActive(true)
                     .build();
